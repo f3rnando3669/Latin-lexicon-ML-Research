b0342b6b3fe7473d29e4fe31429a2367a3f218b9
Theresa Gasser <tgasser@udallas.edu>
2023-03-05 09:23:04 -0600

    added basic relation sorting, see relationtest.txt

--- ProcessingCorpusWords.py
+++ ProcessingCorpusWords.py
@@ -7,7 +7,7 @@ qual = open("textfiles/qualitytest.txt", "w")
 sub = open("textfiles/substancetest.txt", "w")
 act = open("textfiles/actiontest.txt", "w")
 quant = open("textfiles/quantitytest.txt", "w")
-
+rel = open("textfiles/relationtest.txt", "w")
 qual.close()
 sub.close()
 act.close()
@@ -24,12 +24,15 @@ def sort_words(text_file):
 			for word in line.split():
 				tagged_word = unigram_tagger.tag([word])[0]
 	
-				if 'JJ' == tagged_word[1]:
-					qual.write(tagged_word[0]+ " ")
-				if 'NN' == tagged_word[1]: 
-					sub.write(tagged_word[0]+ " ")
-				if 'VB' == tagged_word[1]:
-					act.write(tagged_word[0]+ " ")
+				# if 'JJ' == tagged_word[1]:
+				# 	qual.write(tagged_word[0]+ " ")
+				# if 'NN' == tagged_word[1]: 
+				# 	sub.write(tagged_word[0]+ " ")
+				# if 'VB' == tagged_word[1]:
+				# 	act.write(tagged_word[0]+ " ")
+				if 'JJR' == tagged_word[1] or 'JJS' == tagged_word[1] or 'JJT' == tagged_word[1]: 
+					rel.write(tagged_word[0]+ "\n")
+     
 
 def sort_nums(text_file):
 	with open(text_file, "r") as f: 
@@ -39,5 +42,5 @@ def sort_nums(text_file):
 				if 'CD' == tagged_word[1]:
 					quant.write(tagged_word[0]+ "\n")
 					
-# sort_words("textfiles\allWords.txt")
+sort_words("textfiles/allWords.txt")
 sort_nums("textfiles/allWords.txt")
