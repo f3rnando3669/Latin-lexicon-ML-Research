a7a2e715874343bf88e561fc6c21921b1e814687
Theresa Gasser <tgasser@udallas.edu>
2023-03-26 11:21:55 -0500

    updated

--- spaCY/proposition_list_print.py
+++ spaCY/proposition_list_print.py
@@ -1,5 +1,8 @@
 # shows outpout of np_vp_processing.py
 import pickle
+import spacy
+import explacy
+nlp = spacy.load("en_core_web_sm")
 with open('spaCY\proposition_list.pkl', 'rb') as f: #this method of creating a list of propositions and storing it as a pkl helps save time
 	cur_prop_list = pickle.load(f)
 	f.close()
@@ -8,8 +11,7 @@ with open('spaCY\subj_substance_list.pkl', 'rb') as f: #this method of creating 
 	f.close()
 
 
-for i in cur_prop_list:
-    print(i)
+explacy.print_parse_info(nlp, str(cur_prop_list[0][2]))
     
 # for i in cur_subj_list:
 #     print(i)
