a69ec7275d9589962f04f17322d0b47f15ab9d5f
Anthony Robbins <arobbins@udallas.edu>
2023-06-27 15:18:53 -0500

    Update NLP_Software.java
    - Added PatternMatching and Sentence Separation

--- CoreNLP/NLP_Software/src/main/java/NLP/Software/Pipeline/NLP_Software.java
+++ CoreNLP/NLP_Software/src/main/java/NLP/Software/Pipeline/NLP_Software.java
@@ -1,104 +1,191 @@
-package NLP.Software.Pipeline;
+package NLP_Pipeline;
 
-import edu.stanford.nlp.io.EncodingPrintWriter;
+import com.robbinstony.nlp.Pipeline;
+import com.robbinstony.nlp.SentenceRecognizer;
+import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.pipeline.CoreDocument;
+import edu.stanford.nlp.pipeline.CoreSentence;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
-import org.example.Example.Pipeline;
+import edu.stanford.nlp.simple.Sentence;
+//import edu.stanford.nlp.naturalli.VerbTense;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.util.List;
-import java.util.Scanner;
+import java.lang.reflect.Array;
+import java.util.*;
 
-public class NLP_Software {
-
+public class NLP_Pipeline {
     public static StanfordCoreNLP stanfordCoreNLP = Pipeline.getPipeline();
 
     public static void main(String[] args) throws FileNotFoundException {
 
-        // Scanner reads from input.txt
-        Scanner scanner = new Scanner(new File("C://Users//sansk//IdeaProjects//Example//src//main//java//NLP//Software//Pipeline//input.txt"));
 
-        // Creates an output.txt file to return outputs to
-        PrintWriter out = new PrintWriter("C://Users//sansk//IdeaProjects//Example//src//main//java//NLP//Software//Pipeline//output.txt");
 
-        while (scanner.hasNextLine()) {
-            // Reads each line of input
+        // The following Scanner, named scanner, allows us to read the input.txt file.
+        Scanner scanner = new Scanner(new File("C://Users//apeng//IdeaProjects//NLP_Pipeline//src//main//java//NLP_Pipeline//GutenbergKingJamesBible.txt"));
+
+        // The following PrintWriter, named out, allows us to create and print in the output.txt file.
+        PrintWriter out = new PrintWriter("C://Users//apeng//IdeaProjects//NLP_Pipeline//src//main//java//NLP_Pipeline//output.txt");
+
+        // The following while loop ensures that the Scanner continues to scan until it has nothing to scan.
+        while(scanner.hasNextLine()){
+            // The following String, named input, is where each line from the Scanner will be stored.
             String input = scanner.nextLine();
 
-            // Gives back a count for array size for each sentence
-            int array_count = countWords(input);
+            String lemmaOutput = Lemma(input);
 
-            // Gives each line to the lemma method.
-            String lemma_output = Lemma(input, array_count);
+            String POSoutput = POS(lemmaOutput);
 
-            out.println(lemma_output);
+            String SRoutput = SentenceRecognizer(POSoutput);
 
-            //System.out.println(input);
+            String PatternMatchedOutput = PatternMatching(SRoutput);
+            // the following prints the String named lemmaOutput.
+
+            out.println(PatternMatchedOutput);
         }
-
+        // The following two lines shutdown or close the scanner and output to
         scanner.close();
         out.close();
 
     }
 
-    // Function to count total number of words in the string
-    public static int countWords(String input)
-    {
-
-        // Check if the string is null or empty then return zero
-        if (input == null || input.isEmpty()) {
-            return 0;
-        }
-
-        // Splitting the string around matches of the given regular expression
-        // s is a special character in regular expressions that represents whitespace.
-        String[] words = input.split("[\\s.]+");
-
-        // Check if the last character is a dot, and if so, increment the word count
-        if (input.trim().endsWith(".")) {
-            return words.length + 1;
-        }
-
-        // Return number of words in the given string
-        return words.length;
-    }
-
-    // Takes a string as an input
-    // Return the same string but lemmatized
-    public static String Lemma(String input, int array_length) {
-
-        String[] words = new String[array_length + 2];
-
+    public static String SentenceRecognizer(String input){
         CoreDocument coreDocument = new CoreDocument(input);
 
         stanfordCoreNLP.annotate(coreDocument);
 
-        List<CoreLabel> coreLabelList = coreDocument.tokens();
+        List<CoreSentence> sentences = coreDocument.sentences();
 
-        String lemma = "";
+        StringBuilder output = new StringBuilder();
+        for (CoreSentence sentence : sentences){
+            output.append(sentence.toString()).append("\n");
+        }
+        return output.toString();
+    }
+
+    // The following is the Lemma method that runs the input through the Lemma process of NLP.
+    public static String Lemma(String SRoutput){
+        CoreDocument coreDocument = new CoreDocument(SRoutput);
+        stanfordCoreNLP.annotate(coreDocument);
+
+        List<CoreLabel> coreLabelList = coreDocument.tokens();
+        String[] words = new String[coreLabelList.size()];
 
         int i = 0;
-        for (CoreLabel coreLabel : coreLabelList) {
 
+        // The following for loop Lemma's each element in the array words. "i" is used to only run this as many times as needed, starting with the 0th element.
+        for(CoreLabel coreLabel : coreLabelList){
             words[i] = coreLabel.lemma();
             i++;
-
         }
+        // The following String formats the output with spaces between words, periods are words.
         String output = String.join(" ", words);
+        // The following is self-explanatory. :)
         return output;
     }
 
-    public static String POS(String lemma_output) {
+    public static String POS(String Lemmaoutput){
+        CoreDocument coreDocument = new CoreDocument(Lemmaoutput);
+        stanfordCoreNLP.annotate(coreDocument);
 
-        return lemma_output;
+        List<CoreLabel> coreLabelList = coreDocument.tokens();
+        String[] words = new String[coreLabelList.size()];
+
+        int i = 0;
+
+        for(CoreLabel coreLabel : coreLabelList){
+            words[i] = coreLabel.get(CoreAnnotations.PartOfSpeechAnnotation.class);
+            i++;
+        }
+
+        String output = String.join(" ", words);
+
+        return output;
     }
 
-    public static void PatternCheck() {
-        return;
+    public static String PatternMatching(String POSoutput){
+
+            String[] alphabet = new String[]{"DT", "NN", "NNP", "VBP", "JJ"};
+            int currentState = 0;
+
+            // This should split the sentence in output into individual words in an array.
+            String[] words = POSoutput.split(" ");
+            //System.out.println(words.length);
+
+            // C is for testing.
+            String[] C = new String[]{"DT", "NN", "NNP", "VBP", "JJ"};
+
+            while(currentState != 6 && currentState != 7){
+                if (C[0] == alphabet[0]) {
+                    //System.out.println("Found DT!");
+                    currentState = 1;
+                } else if (C[1] == alphabet[1]) {
+                    //System.out.println("Found NN!");
+                    currentState = 3;
+                } else if (C[2] == alphabet[2]) {
+                    //System.out.println("Found NNP!");
+                    currentState = 4;
+                } else currentState = 7;
+                // If the first tag is "DT", set currentState to 1.
+                // Else if first tag is "NN", set currentState to 3.
+                // Else if first tag is "NNP", set currentState to 4.
+                if(currentState == 1){
+                    if(C[1] == alphabet[1]){
+                        //System.out.println("Found NN after DT!");
+                        currentState = 2;
+                    } else currentState = 7;
+                    // If following the "DT" is "NN", set currentState to 2.
+                }
+                if(currentState == 2){
+                    if(C[2] == alphabet[2]){
+                        //System.out.println("Found VBP!");
+                        currentState = 5;
+                    } else currentState = 7;
+                    // If the following is "VBP", set currentState to 5.
+                }
+                if(currentState == 3){
+                    if(C[2] == alphabet[2]){
+                        //System.out.println("Found VBP!");
+                        currentState = 5;
+                    } else currentState = 7;
+                    // If the following is "VBP", set currentState to 5.
+                }
+                if(currentState == 4){
+                    if(C[2] == alphabet[2]){
+                        //System.out.println("Found VBP!");
+                        currentState = 5;
+                    } else currentState = 7;
+                    //If the following is "VBP", set currentState to 5.
+                }
+                if(currentState == 5){
+                    if(C[4] == alphabet[4]){
+                        //System.out.println("Found NN or JJ!");
+                        currentState = 6;
+                    } else currentState = 7;
+                    // If the following is "NN" or "JJ", set currentState to 6.
+                }
+            }
+            if(currentState == 6){
+                //System.out.println("Pattern Match!");
+                //System.out.println("The words in the sentence that are associated with the POS tags.");
+            }
+            if(currentState == 7){
+                //System.out.println("No pattern found.");
+                return POSoutput;
+
+            }
+        return POSoutput;
     }
 
+    public static int countSentences(String input){
+        if(input == null || input.isEmpty()){
+            return 0;
+        }
 
+        String[] sentences = input.split("[.]+");
+
+        return sentences.length;
+    }
 }
