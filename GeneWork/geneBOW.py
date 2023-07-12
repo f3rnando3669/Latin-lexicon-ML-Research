09891ec08edada67e2a64fe6e7cd03cd3a213254
SebastianG <sgutierrezolvera@udallas.edu>
2023-07-12 16:36:29 -0500

    Put all of the FASTA.txt files in.

--- GeneWork/geneBOW.py
+++ GeneWork/geneBOW.py
@@ -5,19 +5,37 @@ import scipy as sp
 
 # Global Variables
 PATH = os.path.dirname(__name__)
+codonChart = {
+    "TTT": "F", "TTC": "F", "TTA": "L", "TTG": "L", "TCT": "S", "TCC": "S", "TCA": "S",
+    "TCG": "S", "TAT": "Y", "TAC": "Y", "TAA": "X", "TAG": "X", "TGT": "C", "TGC": "C",
+    "TGA": "X", "TGG": "W", "CTT": "L", "CTC": "L", "CTA": "L", "CTG": "L", "CCT": "P",
+    "CCC": "P", "CCA": "P", "CCG": "P", "CAT": "H", "CAC": "H", "CAA": "Q", "CAG": "Q",
+    "CGT": "R", "CGC": "R", "CGA": "R", "CGG": "R", "ATT": "I", "ATC": "I", "ATA": "I",
+    "ATG": "M", "ACT": "T", "ACC": "T", "ACA": "T", "ACG": "T", "AAT": "N", "AAC": "N",
+    "AAA": "K", "AAG": "K", "AGT": "S", "AGC": "S", "AGA": "R", "AGG": "R", "GTT": "V",
+    "GTC": "V", "GTA": "V", "GTG": "V", "GCT": "A", "GCC": "A", "GCA": "A", "GCG": "A",
+    "GAT": "D", "GAC": "D", "GAA": "E", "GAG": "E", "GGT": "G", "GGC": "G", "GGA": "G",
+    "GGG": "G"
+}
 
 def FASTA_Reader(file):
     # FASTA_file.txt -> dictionary
     # reads a FASTA file.txt and separates the Name line from the actual sequence.
     # Also joins the sequence together
     working_file = open(file, "r")
-    #nameTag = working_file.readline().replace('\n',"")
     rv = {'geneName': working_file.readline().replace('\n',""), 'sequence': ""}
     AAseq = ""
     for line in working_file:
             AAseq += line.replace("\n",'')
     rv.update({'sequence':AAseq})
     return rv
+
+def RNA_Check():
+    return
+
+def translation(strand):
+    AAseq = ""
+    return AAseq
 
 trial = FASTA_Reader(os.path.join(PATH, "geneSequences/SbST1.txt"))
 print(trial)
