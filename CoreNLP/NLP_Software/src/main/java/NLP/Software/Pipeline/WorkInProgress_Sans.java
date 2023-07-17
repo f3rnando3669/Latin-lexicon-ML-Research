3e9c1b51ff0ea29fe119a1b39fe3a4f44c34e6bb
sanskritib21 <79542655+sanskritib21@users.noreply.github.com>
2023-07-17 13:11:51 -0500

    Update WorkInProgress_Sans.java

--- CoreNLP/NLP_Software/src/main/java/NLP/Software/Pipeline/WorkInProgress_Sans.java
+++ CoreNLP/NLP_Software/src/main/java/NLP/Software/Pipeline/WorkInProgress_Sans.java
@@ -10,17 +10,13 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.*;
 
-public class Yet_Another {
-
-    // Create a static instance of StanfordCoreNLP pipeline
+public class NLP_Software_CopyTest {
     public static StanfordCoreNLP stanfordCoreNLP = Pipeline.getPipeline();
 
-    // Main method
     public static void main(String[] args) throws FileNotFoundException {
-        // Scanner reads from input.txt
+
         Scanner scanner = new Scanner(new File("C:/Users/sansk/IdeaProjects/NLP_Software/src/main/java/NLP/Software/Pipeline/input.txt"));
 
-        // Creates an output.txt file to return outputs to
         PrintWriter out = new PrintWriter("C:/Users/sansk/IdeaProjects/NLP_Software/src/main/java/NLP/Software/Pipeline/output.txt");
 
         while (scanner.hasNextLine()) {
@@ -33,9 +29,9 @@ public class Yet_Another {
             // Process each sentence individually
             for (String sentence : sentences) {
                 // Declare a variable to store the formatted HashMap contents exclusive to each sentence
-                StringBuilder HashMapOutput = new StringBuilder();
+                StringBuilder IndexPOSMapOutput = new StringBuilder();
 
-                StringBuilder IndexMapOutput = new StringBuilder();
+                StringBuilder IndexLemmaMapOutput = new StringBuilder();
 
                 // Lemmatize the sentence
                 String lemmaOutput = Lemma(sentence);
@@ -43,31 +39,29 @@ public class Yet_Another {
                 // Perform Part-of-Speech tagging on the lemmatized sentence
                 String posOutput = POS(lemmaOutput);
 
-                Map<Integer, String> indexmap = IndexPoSConnect(posOutput);
+                Map<Integer, String> indexposmap = IndexPoSConnect(posOutput);
+
+                Map<Integer, String> indexlemmamap = IndexLemmaConnect(lemmaOutput);
 
                 // Check for specific patterns in the Part-of-Speech tags
-                String patternMatchedOutput = PatternCheck(indexmap);
+                String patternMatchedOutput = PatternCheck(indexposmap);
 
                 // Convert the pattern string to an array
                 String[] patternArray = PatternToArray(patternMatchedOutput);
 
-                // Connect lemmas and POS tags into a map
-                Map<String, String> wordMap = ConnectLemmaPOS(lemmaOutput, posOutput);
-
                 // Find matched words based on the patterns and word map
-                String wordMatch = MatchedWords(patternArray, wordMap);
+                String wordMatch = MatchedWords(patternArray, indexposmap, indexlemmamap);
 
-                // Append each key-value pair from the word map to HashMapOutput
-                for (Map.Entry<String, String> entry : wordMap.entrySet()) {
-                    HashMapOutput.append(entry.getKey()).append(" -> ").append(entry.getValue()).append("\n");
+                for (Map.Entry<Integer, String> entry : indexposmap.entrySet()) {
+                    IndexPOSMapOutput.append(entry.getKey()).append(" -> ").append(entry.getValue()).append("\n");
                 }
 
-                for (Map.Entry<Integer, String> entry : indexmap.entrySet()) {
-                    IndexMapOutput.append(entry.getKey()).append(" -> ").append(entry.getValue()).append("\n");
+                for (Map.Entry<Integer, String> entry : indexlemmamap.entrySet()) {
+                    IndexLemmaMapOutput.append(entry.getKey()).append(" -> ").append(entry.getValue()).append("\n");
                 }
 
                 // Write the results to the output file
-                out.println("Input: " + sentence + "\nPost-Lemma: " + lemmaOutput + "\nPost-POS: " + posOutput + "\nPattern Matching: " + patternMatchedOutput + "\nIndexMapOutput\n" + IndexMapOutput + "\nConnect Output:\n" + HashMapOutput + "Matched Words: " + wordMatch + "\n");
+                out.println("Input: " + sentence + "\nPost-Lemma: " + lemmaOutput + "\nPost-POS: " + posOutput + "\nPattern Matching: " + patternMatchedOutput + "\nIndexPOSMapOutput\n" + IndexPOSMapOutput + "\nIndexLemmaMapOutput:\n" + IndexLemmaMapOutput + "Matched Words: " + wordMatch + "\n");
 
                 // If you want to write the separated sentences to a file, uncomment the following line:
                 // out.println(separatedSentences(lemmaOutput));
@@ -129,23 +123,86 @@ public class Yet_Another {
         return indexmap;
     }
 
+    public static Map<Integer, String> IndexLemmaConnect(String lemmaOutput) {
+        String[] lemmaWords = splitIntoWords(lemmaOutput);
+
+        Map<Integer, String> indexmap = new LinkedHashMap<>();
+
+        for (int i = 0; i < lemmaWords.length; i++) {
+            String value = lemmaWords[i];
+
+            indexmap.put(i, value);
+        }
+
+        return indexmap;
+    }
+
     // Check for specific patterns in the Part-of-Speech tags
     public static String PatternCheck(Map<Integer, String> indexMap) {
-        // Check if the indexMap matches Pattern 1
+        // Check if the indexMap matches Pattern X
         if (isPattern1(indexMap)) {
-            return "DT NN VB JJ"; // Return the matched pattern
-        }
-        // Check if the indexMap matches Pattern 2
-        else if (isPattern2(indexMap)) {
             return "DT NN VBP NN"; // Return the matched pattern
-        }
-        // If neither pattern is found
-        else {
+        } else if (isPattern2(indexMap)) {
+            return "DT NN VB NN";
+        } else if (isPattern3(indexMap)) {
+            return "DT NN VBP JJ";
+        }else if (isPattern4(indexMap)) {
+            return "DT NN VB JJ";
+        }else if (isPattern5(indexMap)) {
+            return "NNP VBP NN";
+        }else if (isPattern6(indexMap)) {
+            return "NNP VB NN";
+        }else if (isPattern7(indexMap)) {
+            return "NNP VBP JJ";
+        }else if (isPattern8(indexMap)) {
+            return "NNP VB JJ";
+        }else if (isPattern9(indexMap)) {
+            return "NN VBP NN";
+        }else if (isPattern10(indexMap)) {
+            return "NN VB NN";
+        }else if (isPattern11(indexMap)) {
+            return "NN VBP JJ";
+        }else if (isPattern12(indexMap)) {
+            return "NN VB JJ";
+        }else { // If neither pattern is found
             return "Pattern Not Found"; // Return the "Pattern Not Found" message
         }
     }
 
     private static boolean isPattern1(Map<Integer, String> indexMap) {
+        // Get the indices for each value in Pattern 1
+        int dtIndex = getIndexForValue(indexMap, "DT");
+        int nnIndex = getIndexAfterValue(indexMap, dtIndex, "NN");
+        int vbpIndex = getIndexAfterValue(indexMap, nnIndex, "VBP");
+        int nn2Index = getIndexAfterValue(indexMap, vbpIndex, "NN");
+
+        // Check if all indices are found in the expected order
+        return dtIndex != -1 && nnIndex != -1 && vbpIndex != -1 && nn2Index != -1;
+    }
+
+    private static boolean isPattern2(Map<Integer, String> indexMap) {
+        // Get the indices for each value in Pattern 2
+        int dtIndex = getIndexForValue(indexMap, "DT");
+        int nnIndex = getIndexAfterValue(indexMap, dtIndex, "NN");
+        int vbIndex = getIndexAfterValue(indexMap, nnIndex, "VB");
+        int nn2Index = getIndexAfterValue(indexMap, vbIndex, "NN");
+
+        // Check if all indices are found in the expected order
+        return dtIndex != -1 && nnIndex != -1 && vbIndex != -1 && nn2Index != -1;
+    }
+
+    private static boolean isPattern3(Map<Integer, String> indexMap){
+        // Get the indices for each value in Pattern 1
+        int dtIndex = getIndexForValue(indexMap, "DT");
+        int nnIndex = getIndexAfterValue(indexMap, dtIndex, "NN");
+        int vbpIndex = getIndexAfterValue(indexMap, nnIndex, "VBP");
+        int jjIndex = getIndexAfterValue(indexMap, vbpIndex, "JJ");
+
+        // Check if all indices are found in the expected order
+        return dtIndex != -1 && nnIndex != -1 && vbpIndex != -1 && jjIndex != -1;
+    }
+
+    private static boolean isPattern4(Map<Integer, String> indexMap){
         // Get the indices for each value in Pattern 1
         int dtIndex = getIndexForValue(indexMap, "DT");
         int nnIndex = getIndexAfterValue(indexMap, dtIndex, "NN");
@@ -156,15 +213,84 @@ public class Yet_Another {
         return dtIndex != -1 && nnIndex != -1 && vbIndex != -1 && jjIndex != -1;
     }
 
-    private static boolean isPattern2(Map<Integer, String> indexMap) {
-        // Get the indices for each value in Pattern 2
-        int dtIndex = getIndexForValue(indexMap, "DT");
-        int nnIndex = getIndexAfterValue(indexMap, dtIndex, "NN");
+    private static boolean isPattern5(Map<Integer, String> indexMap){
+        // Get the indices for each value in Pattern 1
+        int nnpIndex = getIndexForValue(indexMap, "NNP");
+        int vbpIndex = getIndexAfterValue(indexMap, nnpIndex, "VBP");
+        int nnIndex = getIndexAfterValue(indexMap, vbpIndex, "NN");
+
+        // Check if all indices are found in the expected order
+        return nnpIndex != -1 && vbpIndex != -1 && nnIndex != -1;
+    }
+
+    private static boolean isPattern6(Map<Integer, String> indexMap){
+        // Get the indices for each value in Pattern 1
+        int nnpIndex = getIndexForValue(indexMap, "NNP");
+        int vbIndex = getIndexAfterValue(indexMap, nnpIndex, "VB");
+        int nnIndex = getIndexAfterValue(indexMap, vbIndex, "nn");
+
+        // Check if all indices are found in the expected order
+        return nnpIndex != -1 && vbIndex != -1 && nnIndex != -1;
+    }
+
+    private static boolean isPattern7(Map<Integer, String> indexMap){
+        // Get the indices for each value in Pattern 1
+        int nnpIndex = getIndexForValue(indexMap, "NNP");
+        int vbpIndex = getIndexAfterValue(indexMap, nnpIndex, "VBP");
+        int jjIndex = getIndexAfterValue(indexMap, vbpIndex, "JJ");
+
+        // Check if all indices are found in the expected order
+        return nnpIndex != -1 && vbpIndex != -1 && jjIndex != -1;
+    }
+
+    private static boolean isPattern8(Map<Integer, String> indexMap){
+        // Get the indices for each value in Pattern 1
+        int nnpIndex = getIndexForValue(indexMap, "NNP");
+        int vbIndex = getIndexAfterValue(indexMap, nnpIndex, "VB");
+        int jjIndex = getIndexAfterValue(indexMap, vbIndex, "JJ");
+
+        // Check if all indices are found in the expected order
+        return nnpIndex != -1 && vbIndex != -1 && jjIndex != -1;
+    }
+
+    private static boolean isPattern9(Map<Integer, String> indexMap){
+        // Get the indices for each value in Pattern 1
+        int nnIndex = getIndexForValue(indexMap, "NN");
         int vbpIndex = getIndexAfterValue(indexMap, nnIndex, "VBP");
         int nn2Index = getIndexAfterValue(indexMap, vbpIndex, "NN");
 
         // Check if all indices are found in the expected order
-        return dtIndex != -1 && nnIndex != -1 && vbpIndex != -1 && nn2Index != -1;
+        return nnIndex != -1 && vbpIndex != -1 && nn2Index != -1;
+    }
+
+    private static boolean isPattern10(Map<Integer, String> indexMap){
+        // Get the indices for each value in Pattern 1
+        int nnIndex = getIndexForValue(indexMap, "NN");
+        int vbIndex = getIndexAfterValue(indexMap, nnIndex, "VB");
+        int nn2Index = getIndexAfterValue(indexMap, vbIndex, "NN");
+
+        // Check if all indices are found in the expected order
+        return nnIndex != -1 && vbIndex != -1 && nn2Index != -1;
+    }
+
+    private static boolean isPattern11(Map<Integer, String> indexMap){
+        // Get the indices for each value in Pattern 1
+        int nnIndex = getIndexForValue(indexMap, "NN");
+        int vbpIndex = getIndexAfterValue(indexMap, nnIndex, "VBP");
+        int jjIndex = getIndexAfterValue(indexMap, vbpIndex, "JJ");
+
+        // Check if all indices are found in the expected order
+        return nnIndex != -1 && vbpIndex != -1 && jjIndex != -1;
+    }
+
+    private static boolean isPattern12(Map<Integer, String> indexMap){
+// Get the indices for each value in Pattern 1
+        int nnIndex = getIndexForValue(indexMap, "NN");
+        int vbpIndex = getIndexAfterValue(indexMap, nnIndex, "VBP");
+        int jjIndex = getIndexAfterValue(indexMap, vbpIndex, "JJ");
+
+        // Check if all indices are found in the expected order
+        return nnIndex != -1 && vbpIndex != -1 && jjIndex != -1;
     }
 
     private static int getIndexForValue(Map<Integer, String> indexMap, String value) {
@@ -197,52 +323,32 @@ public class Yet_Another {
         return sentence.split("\\s+");
     }
 
-    // Connect lemmas and POS tags into a map
-    public static Map<String, String> ConnectLemmaPOS(String lemmaInput, String posOutput) {
-        String[] lemmaWords = splitIntoWords(lemmaInput);
-        String[] posWords = splitIntoWords(posOutput);
-
-        // Create a LinkedHashMap to store the mapping of lemmaWords to posWords
-        Map<String, String> wordMap = new LinkedHashMap<>();
-
-        // Iterate over the arrays and put the elements into the LinkedHashMap
-        for (int i = 0; i < lemmaWords.length; i++) {
-            String lemma = lemmaWords[i];
-            String pos = i + ". " + posWords[i];
-
-            // Generate a unique key for each word by appending its index
-            String key = i + ". " + lemma;
-
-            // Put the word and its corresponding POS tag into the map
-            wordMap.put(pos, key);
-        }
-
-        // Returning the LinkedHashMap
-        return wordMap;
-    }
-
     // Convert pattern string to an array of words
     public static String[] PatternToArray(String patternOutput) {
         return splitIntoWords(patternOutput);
     }
 
     // Find matched words based on patterns and word map
-    public static String MatchedWords(String[] pattern, Map<String, String> wordMap) {
-        StringBuilder matchedWords = new StringBuilder();
+    public static String MatchedWords(String[] pattern, Map<Integer, String> indexPOSmap, Map<Integer, String> indexLemmamap) {
+        StringBuilder result = new StringBuilder();
 
-        // Iterate over the pattern array
-        for (String patternWord : pattern) {
-            // Iterate over the wordMap entries
-            for (Map.Entry<String, String> entry : wordMap.entrySet()) {
-                if (entry.getValue().equals(patternWord)) {
-                    // Append the matching key to the matchedWords StringBuilder
-                    matchedWords.append(entry.getKey()).append(" ");
-
-                    break; // Break the inner loop to move to the next pattern word
+        for (String word : pattern) {
+            for (Map.Entry<Integer, String> entry : indexPOSmap.entrySet()) {
+                if (word.equals(entry.getValue())) {
+                    Integer key = entry.getKey();
+                    if (indexLemmamap.containsKey(key)) {
+                        String lemma = indexLemmamap.get(key);
+                        result.append(lemma).append(" ");
+                        System.out.println("Match found for word: " + word);
+                        System.out.println("Matching lemma: " + lemma);
+                    }
                 }
             }
         }
 
-        return matchedWords.toString().trim(); // Trim any trailing whitespace and return the result
+        String finalResult = result.toString().trim();
+        System.out.println("Final Result: " + finalResult);
+        return finalResult;
     }
+
 }
