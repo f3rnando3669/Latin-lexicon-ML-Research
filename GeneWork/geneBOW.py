e6fde74754d6956d5bef9ad73d1bbe03efbed56a
SebastianG <sgutierrezolvera@udallas.edu>
2023-07-21 15:43:30 -0500

    - Cleaned up the code - Moved all of the testing functions to the bottom of the file. (might move them into a different file)

--- GeneWork/geneBOW.py
+++ GeneWork/geneBOW.py
@@ -20,22 +20,15 @@ def core_Process(dirName):
     for file in os.listdir(os.path.join(PATH, dirName)):
         if not file.startswith('.'):
             realFile = dirName + "/" + file
-            print("currently working on file {}".format(realFile))
+            #print("currently working on file {}".format(realFile))
             FASTA_reader(os.path.join(PATH, realFile))
+    """
+    Debugging print()s
     print(trigram_coordinates)
     print(gene_coordinates)
     print(data)
+    """
     matrix_factory()
-    return
-def messing_with_matricies():
-    # no input -> no return type
-    # this is just going to print out a matrix as I work on it to get accustomed
-
-
-
-    working_matrix = coo_array((data, (trigram_coordinates, gene_coordinates)), shape=(4, 2))
-    print(working_matrix.toarray())
-
     return
 
 def matrix_factory():
@@ -67,22 +60,20 @@ def gene_indexing(gene):
     global geneIndex
     indexed_Genes.update({gene: geneIndex})
     geneIndex += 1
-    # Do not know if I'll need to reshape this matrix
-    #gene_matrix._shape = (trigramIndex, geneIndex)
-
 
 def array_Update(tri_coord):
     # int -> no return type
     # this updates all 3 arrays that make up our matrix
     global data, gene_coordinates, trigram_coordinates
     data = np.insert(data, len(data),1)
-    print("I am placing {} in the data array".format(1))
+    #print("I am placing {} in the data array".format(1))
+
     gene_coordinates = np.append(gene_coordinates, [(geneIndex-1)])
-    print("I am placing {} in the gene_coordinates array".format((geneIndex-1)))
+    #print("I am placing {} in the gene_coordinates array".format((geneIndex-1)))
+
     # trigram_coordinates is the only one that should be moving around a lot
     trigram_coordinates = np.append(trigram_coordinates, [tri_coord])
-    print("I am placing {} in the trigram_coordinates array".format(tri_coord))
-    print("\n")
+    #print("I am placing {} in the trigram_coordinates array".format(tri_coord))
     return
 
 def trigram_scan(proteinChain):
@@ -96,20 +87,18 @@ def trigram_indexing(trigram):
     # string -> no return type
     # function that adds trigrams that have not been seen to indexed_Trigrams and updates counter
     global trigramIndex
-    if indexed_Trigrams.get(trigram) == None:
+    if indexed_Trigrams.get(trigram) is None:
+        # in the case of a new trigram
         indexed_Trigrams.update({trigram: trigramIndex})
         array_Update(trigramIndex)
         trigramIndex += 1
-        # Do not know if I need to reshape the matrix here
-        # gene_matrix._shape = (trigramIndex, geneIndex)
     else:
-        print("Found a duplicate!")
+        # in the case of a duplicate
         array_Update(indexed_Trigrams.get(trigram))
 
-
-
+####################### TESTING FUNCTIONS #######################
 def my_testing():
-    # Testing
+    # Testing function that serves no purpose anymore
     core_Process("testSequences")
     print(indexed_Trigrams)
     print(indexed_Genes)
@@ -118,10 +107,12 @@ def my_testing():
 
     #trigram_scan(trial2['sequence'])
     #print(trial['sequence'])
+    return
+def messing_with_matricies():
+    # no input -> no return type
+    # just used to figure out how matrices function. No real purpose but being left for future developement sake
+    working_matrix = coo_array((data, (trigram_coordinates, gene_coordinates)), shape=(4, 2))
+    print(working_matrix.toarray())
+    return
 
-
-#my_testing()
-#messing_with_matricies()
 core_Process("testSequences")
-print(indexed_Trigrams)
-print(indexed_Genes)
