b7fda57bff678005695a30f7677200dd1030363e
CLEMENT-ANDI EDET <cedet@udallas.edu>
2023-07-24 23:04:57 -0500

    updated findtop100hypernyms to produce hypernyms and hyponyms of pre-existing hypernyms for top100hypernyms.csv

--- dataset_stuff/findtop100hypernyms.py
+++ dataset_stuff/findtop100hypernyms.py
@@ -1,6 +1,9 @@
 import pandas as pd
-from math import isnan
 from collections import OrderedDict
+from nltk.corpus import wordnet as wn
+from wordnet_refinery import simplify_name
+
+# get the top hypernyms and hyponyms for the top 100 hypernyms
 
 
 def get_hypernym_frequency_map(categories_list):
@@ -21,6 +24,28 @@ def sort_frequency_map(frequency_map):
     return OrderedDict(sorted(frequency_map.items(), key=lambda item: item[1], reverse=True))
 
 
+def get_hypernyms(word_in):
+    word_out = ""
+
+    for word in wn.synsets(word_in):
+        for hypernym in word.hypernyms():
+            word_out += simplify_name(hypernym.name()) + " "
+
+    print(word_out)
+    return word_out
+
+
+def get_hyponyms(word_in):
+    word_out = ""
+
+    for word in wn.synsets(word_in):
+        for hyponym in word.hyponyms():
+            word_out += simplify_name(hyponym.name()) + " "
+
+    print(word_out)
+    return word_out
+
+
 def write_top100(frequency_map):
     set_csv = []
 
@@ -28,10 +53,10 @@ def write_top100(frequency_map):
         if i == 100:
             break
         else:
-            set_csv.append({"top_words": word})
+            set_csv.append({"top_words": word, "hypernyms": get_hypernyms(word), "hyponyms": get_hyponyms(word)})
 
     top100_df = pd.DataFrame.from_dict(set_csv)
-    top100_df.to_csv(r'top100hypernyms.csv', index=False, header=True)
+    top100_df.to_csv(r'wordsandhypernyms.csv', index=False, header=True)
 
 
 df = pd.read_csv("wordsandhypernyms.csv")
