1521b33049a0668105ea05f937f4487bdc0e4c67
Svvmvs <82825432+Svvmvs@users.noreply.github.com>
2023-08-15 15:06:45 -0500

    Update func_main.py
    added comments

--- wiki_stuff/func_main.py
+++ wiki_stuff/func_main.py
@@ -3,6 +3,11 @@ import func_tf_idf_products as ttt
 import sys
 import time
 
+#Given how large wiki_en is and how long all the code together takes, I made this file to run through all the commands needed.
+#So each file got turned into a function and here was where they were all supposed to run, taking 'file' as an input so you could give wiki_short or wiki_en
+
+#This code needs to be updated, I think we need more than top_words and tf_idf might be coming from the wrong file.
+
 file = input("What file should run?")
 
 ftw.top_words(file)
