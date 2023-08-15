5164fd253675fc6e226dde5d03b67165b026e61a
Svvmvs <82825432+Svvmvs@users.noreply.github.com>
2023-08-15 15:25:51 -0500

    Update func_top_words.py
    added comments
    un-hard coded it from 10000 to taking a parameter for the top X

--- wiki_stuff/func_top_words.py
+++ wiki_stuff/func_top_words.py
@@ -1,11 +1,14 @@
 import time
 
-def top_words(myfile):
+#takes a file name and an integer, returns nothing
+#it returns nothing, to save on memeory usage it outputs everything into a text file, I didn't want to pass an object since that would get large fast
+def top_words(myfile, top):
 
     start = time.time()
     file = open(myfile, 'r')
     out = open('func_top_words_out.txt', 'w')
 
+    #count is a dictionary that has given words as keys and the amount of times that word has appeared as a value.
     count = {}
 
     for line in file:
@@ -15,9 +18,11 @@ def top_words(myfile):
             else:
                 count[word] += 1
 
+    #This line sorts the dictionary by items in reverse, so basically the word with the highest number to the word with the lowest.
     sorted_list = sorted(count.items(), key=lambda x:x[1], reverse=True)
 
-    for i in sorted_list[:10000]:
+    #top will limit the list to only the top X words. I was intending to get the top 10,000 words, and then find the related words of just those. 
+    for i in sorted_list[:top]:
         out.write(str(i[0]))
         out.write("\n")
 
