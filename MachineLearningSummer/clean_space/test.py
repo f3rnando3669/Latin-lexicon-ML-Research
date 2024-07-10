ac7de9b916806d75490d797c7590b945032041a7
Clement-Andi Edet <cedet@udallas.edu>
2024-07-10 12:40:25 -0500

    results of test

--- MachineLearningSummer/clean_space/test.py
+++ MachineLearningSummer/clean_space/test.py
@@ -6,15 +6,15 @@ from Prompts.PromptList import PromptList
 from Prompts.SimplePrompt import SimplePrompt
 from Clients.Utilities.FileUtilities import readjson, readfile, write_to_file
 
-# Testing Context-Free Client
-# text = readfile(r"/home/andi/summer2024/clean_space/text.txt")
+# # Testing ContextTight Client
+# text = readfile(r"")
 # prompt = SimplePrompt(text)
 # prompt2 = SimplePrompt("Apply <A> to <y>")
 # prompt3 = SimplePrompt("Apply <C> to <h>")
 # prompts = PromptList()
 # prompts.add_userprompts([prompt, prompt2, prompt3])
 # client_interface = ClientInterface(ContextTightClient)
-# print(client_interface._client.generate(prompts=prompts, json_savepath="jsontext.json"))
+# print(client_interface._client.generate(prompts=prompts, json_savepath=r""))
 
 # Testing ContextTight Client and json writing 
 # prompt = SimplePrompt()
@@ -29,5 +29,5 @@ from Clients.Utilities.FileUtilities import readjson, readfile, write_to_file
 # newjson = readjson("jsontext.json")
 # print(newjson)
 
-r = readjson("jsontext.json")
-write_to_file("response", r[-1])
+# r = readjson("jsontext.json")
+# write_to_file("response", r[-1])
