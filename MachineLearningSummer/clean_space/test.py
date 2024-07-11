113f966a490123699e426034747851cb588aaad0
Liam Hamway <lchamway@gmail.com>
2024-07-10 19:03:16 -0500

    "Deleted sentence_response.txt file and modified test.py to add new code for SimplePrompt and PromptList, and to generate output using ClientInterface."

--- MachineLearningSummer/clean_space/test.py
+++ MachineLearningSummer/clean_space/test.py
@@ -7,16 +7,16 @@ from Prompts.SimplePrompt import SimplePrompt
 from Clients.Utilities.FileUtilities import readjson, readfile, write_to_file
 
 # Testing ContextTight Client
-text = readfile(r"/home/ml/Computer-Science-Research-Summer/MachineLearningSummer/clean_space/text.txt")
-prompt = SimplePrompt(text)
-prompt2 = SimplePrompt("Apply (Apply <A> to <C>) to \"You must avoid medical doctors when ill because otherwise you will become lazy.\"")
-# prompt3 = SimplePrompt("Apply <C> to <h>")
-prompts = PromptList()
-prompts.add_userprompts([prompt, prompt2])
-client_interface = ClientInterface(ContextTightClient)
-# jsonp = r"/home/ml/Computer-Science-Research-Summer/MachineLearningSummer/clean_space/responsehistory.json"
-tpath = r"/home/ml/Computer-Science-Research-Summer/MachineLearningSummer/clean_space/"
-client_interface._client.generate(prompts=prompts, txt_savepath=tpath, txt_name = "response")
+# text = readfile(r"/home/ml/Computer-Science-Research-Summer/MachineLearningSummer/clean_space/text.txt")
+# prompt = SimplePrompt(text)
+# prompt2 = SimplePrompt("Apply (Apply <A> to <C>) to \"You must avoid medical doctors when ill because otherwise you will become lazy.\"")
+# # prompt3 = SimplePrompt("Apply <C> to <h>")
+# prompts = PromptList()
+# prompts.add_userprompts([prompt, prompt2])
+# client_interface = ClientInterface(ContextTightClient)
+# # jsonp = r"/home/ml/Computer-Science-Research-Summer/MachineLearningSummer/clean_space/responsehistory.json"
+# tpath = r"/home/ml/Computer-Science-Research-Summer/MachineLearningSummer/clean_space/"
+# client_interface._client.generate(prompts=prompts, txt_savepath=tpath, txt_name = "response")
 
 # Testing ContextTight Client and json writing 
 # prompt = SimplePrompt()
