e5bbd0a12fd6d76db635694b1c304d1a65e9a9d2
Clement-Andi Edet <cedet@udallas.edu>
2024-07-17 23:57:45 -0500

    we can now count correct response the only issue is that the algorithm depends on the answer being the last tag in the workbook that we have in our rulebook for the tests I have run the correct labels have been extracted

--- MachineLearningSummer/clean_space/classification.py
+++ MachineLearningSummer/clean_space/classification.py
@@ -2,20 +2,38 @@ from Clients.ClientInterface import ClientInterface
 from Clients.ContextTightClient import ContextTightClient
 from Prompts.SimplePrompt import SimplePrompt
 from Prompts.PromptList import PromptList
-from Clients.Utilities.FileUtilities import readfile, readjson
+from Clients.Utilities.FileUtilities import readfile, readjson, write_to_file_in_dir
+from extract import iscorrect
 
 # Classification Experiment
-rulebook_path = r"/home/andi/summer2024/Computer-Science-Research-Summer/MachineLearningSummer/rule_book_bank/RAW_Rulebooks_15.txt"
+rulebook_path = r"/home/andi/summer2024/Computer-Science-Research-Summer/MachineLearningSummer/rule_book_bank/RAW_Rulebooks_17.txt"
 params = readfile(rulebook_path)
 param_prompt = SimplePrompt(params)
 mapping_path = r"/home/andi/summer2024/Computer-Science-Research-Summer/MachineLearningSummer/fallacy_dataset/article_to_label_test.json"
 article_to_label_map = readjson(mapping_path)
 # print(article_to_label_map)
-for article in article_to_label_map:
-    print(article_to_label_map[article])
-    prompt = SimplePrompt(f"Apply <IDAA> to \"{article}\"")
-    prompts = PromptList()
-    prompts.add_userprompts([param_prompt, prompt])
-    client_interface = ClientInterface(ContextTightClient)
-    tpath = r"/home/andi/summer2024/Computer-Science-Research-Summer/MachineLearningSummer/clean_space/response_bank"
-    client_interface._client.generate(prompts=prompts, txt_savepath=tpath, txt_name = article_to_label_map[article])
+# target = ['<FU>', '<WCB>']
+response_paths = []
+# for article in article_to_label_map:
+#     prompt = SimplePrompt(f"Apply <IDAA> to \"{article}\"")
+#     prompts = PromptList()
+#     prompts.add_userprompts([param_prompt, prompt])
+#     client_interface = ClientInterface(ContextTightClient)
+#     tpath = r"/home/andi/summer2024/Computer-Science-Research-Summer/MachineLearningSummer/clean_space/response_bank"
+#     response = client_interface._client.generate(prompts=prompts)
+#     txt_name = article_to_label_map[article][1:-1]
+#     path = write_to_file_in_dir(tpath, txt_name, response)
+#     response_paths.append(path)
+
+import os
+dir = r"/home/andi/summer2024/Computer-Science-Research-Summer/MachineLearningSummer/clean_space/response_bank/"
+response_paths = os.listdir(dir)
+count = 0
+total = 0
+for path in response_paths:
+    correct, num =  iscorrect(dir+path)
+    if correct:
+        count += 1
+    total += num
+
+print(f'Correctly Indentified: {count}\nPercentage: {count/total*100}')
