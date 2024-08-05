6a6a5b00d334e083ad05e7e30d0b6d5707b18c7d
Sebas-2000 <sgutierrezolvera@udallas.edu>
2024-08-05 15:39:14 -0500

    ran experiments + modularized procedures

--- MachineLearningSummer/clean_space/classification.py
+++ MachineLearningSummer/clean_space/classification.py
@@ -2,31 +2,41 @@ from Clients.ClientInterface import ClientInterface
 from Clients.ContextTightClient import ContextTightClient
 from Prompts.SimplePrompt import SimplePrompt
 from Prompts.PromptList import PromptList
-from Clients.Utilities.FileUtilities import readfile, readjson, write_to_file_in_dir, readcsv
+from Clients.Utilities.FileUtilities import readfile, write_to_file_in_dir
 from extract import iscorrect, iscorrect_text
 
-# Classification Experiment
-def batch_classify(rbk_path: str, article_to_label_map, batch_dir:str, summary_name='batch_summary'):
+def batch_classify(rbk_path: str, article_to_label_map:dict, batch_dir:str, summary_name='batch_summary')->str:
     params = readfile(rbk_path)
-    # portfolio = readfile(portfolio_path)
-    # param_prompt = SimplePrompt(params+'\n\n'+portfolio)
-    # print(param_prompt)
-    # return
-    
+    response_paths = classify(article_to_label_map=article_to_label_map, preprompt=params, batch_dir=batch_dir)
+    summary = prepare_summary(response_paths=response_paths, batch_dir=batch_dir, summary_name=summary_name, rulebook=rbk_path)
+    return summary
+
+def batch_classify_with_portfolio(rbk_path: str, portfolio_path:str, article_to_label_map:dict, batch_dir:str, summary_name='batch_summary')->str:
+    params = readfile(rbk_path)
+    portfolio = readfile(portfolio_path)    
+    response_paths = classify(article_to_label_map=article_to_label_map, preprompt=params+portfolio, batch_dir=batch_dir)
+    summary = prepare_summary(response_paths=response_paths, batch_dir=batch_dir, summary_name=summary_name, rulebook=rbk_path+' and '+portfolio_path)
+    return summary
+
+def generate_response(preprompt: str, article: str) -> str:
+    prompt = SimplePrompt(preprompt+'\n'+f"\nApply <IDAA> to \"{article}\"")
+    prompts = PromptList()
+    prompts.add_userprompts([prompt])
+    client_interface = ClientInterface(ContextTightClient)
+    client = client_interface._client
+    response = client.generate(prompts=prompts)
+    return response
+
+def classify(article_to_label_map: dict, preprompt: str, batch_dir: str) -> list[str]:
     response_paths = []
     print_freq  = len(article_to_label_map) // 10
     counter = 0
     index = 1
+
     for article in article_to_label_map:
         if article == 'article':
             continue
-
-        prompt = SimplePrompt(params+'\n'+f"\nApply <IDAA> to \"{article}\"")
-        prompts = PromptList()
-        prompts.add_userprompts([prompt])
-        client_interface = ClientInterface(ContextTightClient)
-        client = client_interface._client
-        response = client.generate(prompts=prompts)
+        response = generate_response(preprompt=preprompt, article=article)
         txt_name = article_to_label_map[article][1:-1]
         path = write_to_file_in_dir(batch_dir, txt_name, response)
         response_paths.append(path)
@@ -34,10 +44,12 @@ def batch_classify(rbk_path: str, article_to_label_map, batch_dir:str, summary_n
         if counter % print_freq == 0:
             print("="*10, f'group_{index}_complete', '='*10 )
             index += 1
-        
     if counter % print_freq != 0:
         print("="*10, f'group_{index}_complete', '='*10 )
-        
+    
+    return response_paths
+
+def prepare_summary(response_paths, batch_dir, summary_name, rulebook)->str:
     category_details = {}
     incorrect_list = []
     count = 0
@@ -47,7 +59,6 @@ def batch_classify(rbk_path: str, article_to_label_map, batch_dir:str, summary_n
             category_details[category]['Total'] += 1
         else:
             category_details[category] = {'Total':1, 'Correct':0}
-
         correct = iscorrect(path)
         if correct:
             count += 1
@@ -66,41 +77,21 @@ def batch_classify(rbk_path: str, article_to_label_map, batch_dir:str, summary_n
         summary += f'\n{category}:\nCorrect: {correct}\nTotal: {total}\nPercentage: {correct/total*100}\n'
         summary += "=" * 30
     summary += "\n\nPaths for Incorrect Files:\n"+"\n".join(incorrect_list)
-    write_to_file_in_dir(batch_dir, summary_name, summary, text_analyzed=rbk_path)
-    print('Done')
+    write_to_file_in_dir(batch_dir, summary_name, summary, text_analyzed=rulebook)
     return summary
-
-# rbk_path = r"MachineLearningSummer/rule_book_bank/RAW_RuleBooks_23.txt"
-# mapping_path = r"MachineLearningSummer/fallacy_dataset/article_to_label_test.json"
-# dataset_path = r'MachineLearningSummer/fallacy_dataset/datasets/30%_of_80%_of_70%_of_dataset.csv'
-# dataset_path = r'MachineLearningSummer/fallacy_dataset/article_to_label_test.json'
-# batch_dir = r"MachineLearningSummer/clean_space/response_bank/batch3"
-# batch_classify(rbk_path=rbk_path, dataset_path=dataset_path, batch_dir=batch_dir)
 
 def build_portfolio(rbk_path: str, article_to_label_map: dict, portfolio_dir: str, portfolio_name: str) -> str:
     params = readfile(rbk_path)
     path = f'{portfolio_dir}/{portfolio_name}.txt'
     fd = open(path, 'a')
-    # print(article_to_label_map)
-    # return ''
-    for i, article in enumerate(article_to_label_map):
+    for article in article_to_label_map:
         if article == 'article':
             continue
-        # print(article_to_label_map[article])
-        # break
-        prompt = SimplePrompt(params+f"\nApply <IDAA> to \"{article}\"")
-        prompts = PromptList()
-        prompts.add_userprompts([prompt])
-        client_interface = ClientInterface(ContextTightClient)
-        client = client_interface._client
-        response = client.generate(prompts=prompts)
+        response = generate_response(preprompt=params, article=article)
         tag = article_to_label_map[article]
         correct = iscorrect_text(correct_tag=tag, text=response)
-        # print(i)
         if correct:
             fd.write(response)
             fd.write(f'\nType of Defective Argument for \'{article}\': {tag}\n\n')
-        # break
     fd.close()
-    print('Portfolio is ready')
     return path
--- MachineLearningSummer/clean_space/driver.py
+++ MachineLearningSummer/clean_space/driver.py
@@ -1,83 +1,76 @@
 from addmore_rulebook_examples import add_examples
-from sampledataset import partition_dataset, get_training_data, get_test_data
-from classification import batch_classify, build_portfolio
-import os
-from Clients.Utilities.FileUtilities import write_dict_to_csv_in_dir, readjson, write_tojson
-import time
-import re
+from sampledataset import get_new_data
+from classification import batch_classify, build_portfolio, batch_classify_with_portfolio
+from driver_utilities import update_data, generate_batch_directory, write_experiment_info, build_article_label_map, get_rulebook_bank_path, get_clean_space_dir
+from Clients.Utilities.FileUtilities import readjson
 
-def cli_stuff() -> str:
-    current_dir = os.getcwd()
-    dirs = os.listdir(f'{current_dir}/MachineLearningSummer/clean_space/response_bank')
-    count = 1
-    for dir in dirs:
-        if re.match(r'batch[0-9]+', dir):
-            count += 1
+def experiment(train_n:int, test_n:int, dataset_path, rbk_path, create_new_rbk=False, portfolio=False, portfolio_path="", forbidden_examples={}, train_data={}, test_data={}, indexes={}, select_labels={}) -> str:
+    """
+    run a classification experiment\n
+    returns summary path
+    """
+    if portfolio:
+        if not portfolio_path:
+            raise Exception("No portfolio path defined")
+    extra_train_data, updated_indexes = get_new_data(dataset_path, train_n, indexes, forbidden_examples, select_labels)
+    train_data = update_data(data_dict=train_data, new_data=extra_train_data)
+    extra_test_data, updated_indexes = get_new_data(dataset_path, test_n, indexes, forbidden_examples, select_labels)
+    indexes.update(updated_indexes)
+    test_data = update_data(data_dict=test_data, new_data=extra_test_data)
+    working_directory = generate_batch_directory()
+    write_experiment_info(working_dir=working_directory, data_dicts=[('indexes', indexes), ('test_data', test_data), ('train_data', train_data)])
+    
+    if create_new_rbk:
+        rbk_path = add_examples(train_data, rbk_path, rule_book_bank_path, 'RAW_RuleBooks')
+    article_to_label_map = build_article_label_map(test_data)
+    
+    decision = input('Continue? [Y]/[n]: ')
+    summary = ""
+    if decision == 'Y' or decision == 'y':
+        if portfolio:
+            print('classifying with portfolio...')
+            summary = batch_classify_with_portfolio(rbk_path=rbk_path, portfolio_path=portfolio_path, article_to_label_map=article_to_label_map, batch_dir=working_directory)
+        else:
+            print('classifying...')
+            summary = batch_classify(rbk_path=rbk_path, article_to_label_map=article_to_label_map, batch_dir=working_directory)
+    print("Experiment complete!")
+    return summary
 
-    core = current_dir+f'/MachineLearningSummer/clean_space/response_bank/batch{count}'
-    os.system(f'mkdir -p {core}')
-    # test_data_path = write_dict_to_csv_in_dir(core, 'test_data.csv', test_list, ['label', 'article'])
-    # os.system(f'wc -w {current_dir}/{new_rbk_path} {test_data_path} > {core}/wc_stats.txt')
+def create_portfolio(portfolio_dir:str, portfolio_name:str, dataset_path:str, rbk_path:str, data_dict:dict, example_count:int, indexes={}, forbidden_examples={}, select_labels={}) -> str:
+    """
+    create a portfolio\n
+    returns portfolio path
+    """
+    extra_data,_ = get_new_data(dataset_path, example_count, indexes, forbidden_examples, selected=select_labels)
+    data_dict = update_data(data_dict=data_dict, new_data=extra_data)
+    article_to_label_map = build_article_label_map(data_dict)
+    decision = input('Continue? [Y]/[n]: ')
+    if decision == 'Y' or decision == 'y':
+        print('building portfolio...')
+        portfolio_path = build_portfolio(rbk_path=rbk_path, article_to_label_map=article_to_label_map, portfolio_dir=portfolio_dir, portfolio_name=portfolio_name)
+        print('Portfolio complete!')
+    return portfolio_path
 
-    # print(f'Check: {core}/wc_stats.txt\nContains word count stats\n')
-    # time.sleep(0.2)    
-    return core
+# need to be sure of the side effects here...
+# def create_portolio_and_classify(portfolio_dir: str, portfolio_name: str, train_n:int, test_n:int, rbk_path:str, dataset_path:str, train_data={}, test_data={}, indexes={}, select_labels={}, forbidden_examples={}):
+#     portfolio_path = create_portfolio(portfolio_dir=portfolio_dir, portfolio_name=portfolio_name, dataset_path=dataset_path, rbk_path=rbk_path, data_dict=test_data, example_count=test_n, forbidden_examples=forbidden_examples)
+#     experiment(train_n=train_n, test_n=test_n, dataset_path=dataset_path, rbk_path=rbk_path, portfolio=True, portfolio_path=portfolio_path, forbidden_examples=forbidden_examples, train_data=train_data, test_data=test_data, indexes=indexes, select_labels=select_labels)
 
 train_n = 0
-test_n = 4
-
+test_n = 8
+clean_space_dir = get_clean_space_dir()
 dataset_path = r'MachineLearningSummer/fallacy_dataset/datasets/70%_of_dataset.csv'
 forbidden_examples = readjson(r'MachineLearningSummer/rulebook_intermediates/examples.json')
-train_data: dict = readjson(r'MachineLearningSummer/clean_space/response_bank/batch4/train_data.json')
-# test_data: dict = readjson(r'MachineLearningSummer/clean_space/response_bank/batch4/test_data.json')
+# train_data: dict = readjson(clean_space_dir+r'/response_bank/batch4/train_data.json')
+# test_data: dict = readjson(clean_space_dir+r'response_bank/batch4/test_data.json')
+# indexes: dict = readjson(clean_space_dir+r'/response_bank/batch4/indexes.json')
 select_labels = {'<IR>', '<FE>', '<RR>', '<G>', '<DEP>', '<FU>', '<WCB>'}
-test_data = {label:[] for label in select_labels}
-indexes: dict = readjson(r'MachineLearningSummer/clean_space/response_bank/batch4/indexes.json')
-# select_labels = {'<FU>', '<DEP>', '<RR>'}
+indexes = {label:0 for label in select_labels}
+rule_book_bank_path = get_rulebook_bank_path()
+rbk_path = f'{rule_book_bank_path}/RAW_RuleBooks_36.txt'
+portfolio_dir = clean_space_dir+'/Portfolios'
+portfolio_name = r'portfolio3'
 
-extra_train_data, updated_indexes = get_training_data(dataset_path, train_n, indexes, forbidden_examples, select_labels)
-for label in extra_train_data:
-    if label in train_data:
-        train_data[label] += extra_train_data[label]
-    else:
-        train_data[label] = extra_train_data[label]
-
-extra_test_data, updated_indexes = get_test_data(dataset_path, test_n, indexes, forbidden_examples)
-indexes.update(updated_indexes)
-
-count = 0
-for label in extra_test_data:
-    test_data[label] += extra_test_data[label]
-    count += len(test_data[label])
-    # print(f'{label} count: ', len(test_data[label]))
-print('count: ', count)
-# print(test_data)
-# c = input()
-write_tojson(r'MachineLearningSummer/clean_space/response_bank/batch34/indexes.json', indexes)
-write_tojson(r'MachineLearningSummer/clean_space/response_bank/batch34/test_data.json', test_data)
-write_tojson(r'MachineLearningSummer/clean_space/response_bank/batch34/train_data.json', train_data)
-
-# new_path = r'MachineLearningSummer/rule_book_bank'
-# name = 'RAW_RuleBooks'
-rbk_path = r'MachineLearningSummer/rule_book_bank/RAW_RuleBooks_36.txt'
-portfolio_dir = r'MachineLearningSummer/clean_space/Portfolios'
-portfolio_name = r'portfolio2.txt'
-
-# new_rbk_path = add_examples(train_data, rbk_path, new_path, name)
-new_rbk_path = rbk_path
-decision = input('Continue? [Y]/[n]: ')
-if decision == 'Y' or decision == 'y':
-    print('running...')
-    batch_dir = r"MachineLearningSummer/clean_space/response_bank/batch34"
-    article_to_label_map = {}
-    for label in test_data:
-        for article in test_data[label]:
-            article_to_label_map[article] = label
-    batch_classify(rbk_path=new_rbk_path, article_to_label_map=article_to_label_map, batch_dir=batch_dir)
-
-# article_to_label_map = {}
-# for label in test_data:
-#     for article in test_data[label]:
-#         article_to_label_map[article] = label
-# # print(article_to_label_map)
-# print(build_portfolio(rbk_path=rbk_path, article_to_label_map=article_to_label_map, portfolio_dir=portfolio_dir, portfolio_name='portfolio2'))
+portfolio_path = r'MachineLearningSummer/clean_space/Portfolios/portfolio3.txt'
+experiment(train_n=train_n, test_n=test_n, dataset_path=dataset_path, rbk_path=rbk_path, forbidden_examples=forbidden_examples, indexes=indexes, select_labels=select_labels, portfolio=True, portfolio_path=portfolio_path)
+# create_portfolio(portfolio_dir=portfolio_dir, portfolio_name=portfolio_name, dataset_path=dataset_path, rbk_path=rbk_path, data_dict={}, example_count=test_n,forbidden_examples=forbidden_examples, indexes=indexes, select_labels=select_labels)
