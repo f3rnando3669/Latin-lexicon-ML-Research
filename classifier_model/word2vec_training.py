135781d691e7e16713cd75f2fe28b1353ca48404
Liam Hamway <lchamway@gmail.com>
2024-08-07 11:13:42 -0500

    Added in Word2Vec Model finished

--- classifier_model/word2vec_training.py
+++ classifier_model/word2vec_training.py
@@ -6,8 +6,13 @@ csv_path = r'C:\Users\Liam\Desktop\Summer Research\Computer-Science-Research-Sum
 train_data = pd.read_csv(csv_path)
 
 
-sequences = [list(seq) for seq in train_data['sequence'].values]
+#sq = [list(seq) for seq in train_data['Sequence'].values]
+
+# Prepare sentences for Word2Vec
+sequences = train_data[' Sequence'].apply(list).tolist()
 
 # Train Word2Vec model
 word2vec_model = Word2Vec(sequences, vector_size=100, window=5, min_count=1, sg=1, epochs=10)
-word2vec_model.save('word2vec.model')
+model_path = r'C:\Users\Liam\Desktop\Summer Research\Computer-Science-Research-Summer\word2vecProtein.model'
+word2vec_model.save(model_path)
+print(f"Model saved to {model_path}")
